616c9c6ee480c5f500038ba2cf334e56c419f6bc
Phoebe Lau <phoebelau7@gmail.com>
2017-12-04 20:42:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d\d{3}d{3}d{4}\d/)
-    return true
-  elsif phone.scan(/\d\d{3}-d{3}-d{4}\d/)
+  if phone.scan(/\d\d{3}d{3}d{4}\d/) || phone.scan(/\d\d{3}-d{3}-d{4}\d/)
     true
   else
     false
